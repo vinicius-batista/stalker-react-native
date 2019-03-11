4d4dccae02fd6d5f8d7d4de634d9d0dbeac5f0e3
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-03-11 08:57:34 -0300

    add react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,21 @@
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import Home from './screens/Home'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-})
-
-interface Props {}
-export default class App extends Component<Props> {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    )
+const AppNavigator = createStackNavigator(
+  {
+    Home
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'Stalker'
+    }
   }
+)
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default function App () {
+  return <AppContainer />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
